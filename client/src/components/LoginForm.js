fa451c23a09f302a4fdba56c5fb19829847ec27b
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-10-10 18:03:50 +1100

    Replace the loginUser() functionality imported from the API file with the LOGIN_USER mutation functionality

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,92 +1,109 @@
-// see SignupForm.js for comments
-import React, { useState } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
 
-import { loginUser } from '../utils/API';
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
+
+// Import the `useMutation()` hook from Apollo Client
+import { useMutation } from "@apollo/client";
+// Import the GraphQL mutation
+import { LOGIN_USER } from "../../utils/mutations";
 
 const LoginForm = () => {
-  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
-  const [validated] = useState(false);
-  const [showAlert, setShowAlert] = useState(false);
+	// Set initial form state
+	const [userFormData, setUserFormData] = useState({ email: "", password: "" });
+	// Set state for form validation
+	const [validated] = useState(false);
+	// Set state for alert
+	const [showAlert, setShowAlert] = useState(false);
+	// Invoke `useMutation()` hook to return a Promise-based function and data about the LOGIN_USER mutation
+	const [loginUser] = useMutation(LOGIN_USER);
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
-  };
+	const handleInputChange = (event) => {
+		const { name, value } = event.target;
+		setUserFormData({ ...userFormData, [name]: value });
+	};
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
+	const handleFormSubmit = async (event) => {
+		event.preventDefault();
 
-    // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+		// Check if form has everything (as per react-bootstrap docs)
+		const form = event.currentTarget;
+		if (form.checkValidity() === false) {
+			event.preventDefault();
+			event.stopPropagation();
+		}
 
-    try {
-      const response = await loginUser(userFormData);
+		// Since mutation function is async, wrap in a `try...catch` to catch any network errors from throwing due to a failed request
+		try {
+			// Execute mutation and pass in defined parameter data as variables
+			const { data } = await loginUser({
+				variables: { userFormData },
+			});
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+			console.log(user);
+			Auth.login(data.loginUser.token);
+		} catch (err) {
+			console.error(err);
+			setShowAlert(true);
+		}
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
+		setUserFormData({
+			username: "",
+			email: "",
+			password: "",
+		});
+	};
 
-    setUserFormData({
-      username: '',
-      email: '',
-      password: '',
-    });
-  };
+	return (
+		<>
+			<Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+				<Alert
+					dismissible
+					onClose={() => setShowAlert(false)}
+					show={showAlert}
+					variant="danger"
+				>
+					Something went wrong with your login credentials!
+				</Alert>
+				<Form.Group>
+					<Form.Label htmlFor="email">Email</Form.Label>
+					<Form.Control
+						type="text"
+						placeholder="Your email"
+						name="email"
+						onChange={handleInputChange}
+						value={userFormData.email}
+						required
+					/>
+					<Form.Control.Feedback type="invalid">
+						Email is required!
+					</Form.Control.Feedback>
+				</Form.Group>
 
-  return (
-    <>
-      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-          Something went wrong with your login credentials!
-        </Alert>
-        <Form.Group>
-          <Form.Label htmlFor='email'>Email</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Your email'
-            name='email'
-            onChange={handleInputChange}
-            value={userFormData.email}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
-        </Form.Group>
-
-        <Form.Group>
-          <Form.Label htmlFor='password'>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Your password'
-            name='password'
-            onChange={handleInputChange}
-            value={userFormData.password}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-        </Form.Group>
-        <Button
-          disabled={!(userFormData.email && userFormData.password)}
-          type='submit'
-          variant='success'>
-          Submit
-        </Button>
-      </Form>
-    </>
-  );
+				<Form.Group>
+					<Form.Label htmlFor="password">Password</Form.Label>
+					<Form.Control
+						type="password"
+						placeholder="Your password"
+						name="password"
+						onChange={handleInputChange}
+						value={userFormData.password}
+						required
+					/>
+					<Form.Control.Feedback type="invalid">
+						Password is required!
+					</Form.Control.Feedback>
+				</Form.Group>
+				<Button
+					disabled={!(userFormData.email && userFormData.password)}
+					type="submit"
+					variant="success"
+				>
+					Submit
+				</Button>
+			</Form>
+		</>
+	);
 };
 
 export default LoginForm;
