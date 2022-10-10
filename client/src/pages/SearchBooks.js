00fd67074ed7818afde0dfa093e86332c7ca2045
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-10-11 00:35:50 +1100

    Change bookToSave to input

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -81,9 +81,7 @@ const SearchBooks = () => {
 		}
 
 		try {
-			await saveBook({
-				variables: { ...bookToSave },
-			});
+			await saveBook({ variables: { input: bookToSave } });
 
 			// If book successfully saves to user's account, save book id to state
 			setSavedBookIds([...savedBookIds, bookToSave.bookId]);
